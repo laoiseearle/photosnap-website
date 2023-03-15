b3d5db1df0775d7f9f7c4448f1d8cd5b0d060b36
Laoise Earle <laoiseearle92@gmail.com>
2023-03-15 14:46:13 +0000

    Add overlay after opening menu

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const hamburger = document.getElementById('ham');
 const menu = document.getElementById('menu');
+const mainContent = document.getElementsByTagName('main')[0];
 
 const menuToggle = () => {
   if (menu.classList.contains('menu-open')) {
     hamburger.src = 'assets/shared/mobile/menu.svg';
     menu.classList.remove('menu-open');
     document.body.style.overflow = 'visible';
+    mainContent.classList.remove('overlay');
   } else {
     hamburger.src = 'assets/shared/mobile/close.svg';
     menu.classList.add('menu-open');
     document.body.style.overflow = 'hidden';
+    mainContent.classList.add('overlay');
   }
 };
