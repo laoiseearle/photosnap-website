60d71dc9addaf598d580404e2206e68375e07083
Laoise Earle <laoiseearle92@gmail.com>
2023-03-18 00:42:58 +0000

    Create tablet design

--- script.js
+++ script.js
@@ -2,16 +2,30 @@ const hamburger = document.getElementById('ham');
 const menu = document.getElementById('menu');
 const mainContent = document.getElementsByTagName('main')[0];
 
+const closeMenu = () => {
+  hamburger.src = 'assets/shared/mobile/menu.svg';
+  menu.classList.remove('menu-open');
+  document.body.style.overflow = 'visible';
+  mainContent.classList.remove('overlay');
+};
+
+const openMenu = () => {
+  hamburger.src = 'assets/shared/mobile/close.svg';
+  menu.classList.add('menu-open');
+  document.body.style.overflow = 'hidden';
+  mainContent.classList.add('overlay');
+};
+
+window.addEventListener('resize', e => {
+  if (e.target.innerWidth >= 768) {
+    closeMenu();
+  }
+});
+
 const menuToggle = () => {
   if (menu.classList.contains('menu-open')) {
-    hamburger.src = 'assets/shared/mobile/menu.svg';
-    menu.classList.remove('menu-open');
-    document.body.style.overflow = 'visible';
-    mainContent.classList.remove('overlay');
+    closeMenu();
   } else {
-    hamburger.src = 'assets/shared/mobile/close.svg';
-    menu.classList.add('menu-open');
-    document.body.style.overflow = 'hidden';
-    mainContent.classList.add('overlay');
+    openMenu();
   }
 };
